0746acdccc8aa53fb93fb07e8ac1fd5a6dd27d8c
dev9318 <dev.moxaj@gmail.com>
2022-09-07 23:53:20 +0530

    add ISA image to website

--- src/pages/haCouncil/HA2223.jsx
+++ src/pages/haCouncil/HA2223.jsx
@@ -134,11 +134,11 @@ function HA2223() {
               <div class="col m-auto">
                 <div class=" h-100 overflow-hidden  rounded-4" >
                   <div class="d-flex flex-column h-100 p-4 pb-3 text-shadow-1">
-                    <img src={"./21-22/sudhanshu.jpeg"} alt="Bootstrap" width="75%" style={{contentFit:"cover"}}  class="rounded-circle border border-white m-auto "/>
+                    <img src={"./22-23/DevDesai.jpg"} alt="Bootstrap" width="75%" style={{contentFit:"cover"}}  class="rounded-circle border border-white m-auto "/>
                      <ul class="d-flex list-unstyled mt-auto">
                       <li class="m-auto py-2">
                         <div className='d-flex flex-column m-auto '>
-                            <h4 className="name text-primary text-center">Dev Patel</h4>
+                            <h4 className="name text-primary text-center">Dev Moxaj Desai</h4>
                             <h6 className="text-white mt-2">Institute System Administrator (ISA)</h6>
                             <h7 className="text-white"><span>Email:</span> 180100115@iitb.ac.in</h7>
                             <h7 className="text-white"><span>Phone:</span> 7667557469</h7>
